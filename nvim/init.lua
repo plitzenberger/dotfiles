b6db8b3e2e6e5c6c467e3a67ee1ae42330179e46
Philipp Litzenberger <philipp@bendcircular.com>
2026-03-13 18:41:23 +0100

    feat(nvim): add jk esc keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -157,6 +157,7 @@ vim.opt.scrolloff = 10
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
